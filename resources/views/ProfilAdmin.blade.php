dad3001312b40abc2a75c414aa58a394c521a82e
Chaymae Benayad <chaymae.benayad@etu.uae.ac.ma>
2021-02-21 22:08:12 +0100

    add components

--- resources/views/ProfilAdmin.blade.php
+++ resources/views/ProfilAdmin.blade.php
@@ -1,6 +1,3 @@
-@php
-use \App\Http\Controllers\UserController;
-@endphp
 <!doctype html>
 <html lang="en">
   <head>
@@ -20,58 +17,9 @@ use \App\Http\Controllers\UserController;
   </head>
 
   <body>
-    <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="/img/logo.png" alt="logo" height="130"/></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-        </li>  
-        <li style="padding-right:25px" class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fa fa-bell"></i>
-          <span class="badge rounded-pill badge-notification bg-danger">{{UserController::count_notifications()}}</span>
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <li><a style="color:blue" class="dropdown-item" href="{{route('markRead')}}">Marquer Tout Comme Lu</a></li>
-            {{ UserController::notification_list() }}
-          </ul>
-        </li>
-        <li style="padding-right:20px" class="nav-item">
-          <a class="nav-link" href="#"> <i style="padding-right:10px" class="fas fa-user-tie"></i>Bonjour Admin</a>
-        </li>
-        <li class="nav-item">
-          <a id="logoutBtn" class="nav-link" href="#"><i style="padding-right:10px" class="fas fa-sign-out-alt"></i>Se déconnecter</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<!-- End Navbar -->
 
-<!-- SideBar -->
-    <div class="sideBar">
-      <!--Navigation bar-->
-        <ul class="navig">
-            <li><a href="#"><i class="fas fa-home"></i>Accueil</i></a></li>
-            <li><a href="#"><i class="fas fa-plus-circle"></i>Ajouter Document</i></a></li>
-            <li><a href="#"><i class="fas fa-file-alt"></i>Documents</i></a></li>
-            <li><a href="#"><i class="fas fa-users"></i>Employés</i></a></li>
-            <li><a href="#"><i class="fas fa-file-archive"></i>Documents Archivés</i></a></li>
-        </ul>
-      <!--Navigation bar end-->
-       <!--Copyright-->
-       <div class="copyright">
-        Copyright 2021&copy;Developed By SMACeDOC Team
-      </div>
-      <!--Copyright end-->
-<!-- End SideBar -->
-   
+   <x-navbar />
+   <x-sidebar />
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <!-- SideBar -->
+    <div class="sideBar">
+      <!--Navigation bar-->
+        <ul class="navig">
+            <li><a href="#"><i class="fas fa-home"></i>Accueil</i></a></li>
+            <li><a href="#"><i class="fas fa-plus-circle"></i>Ajouter Document</i></a></li>
+            <li><a href="#"><i class="fas fa-file-alt"></i>Documents</i></a></li>
+            <li><a href="#"><i class="fas fa-users"></i>Employés</i></a></li>
+            <li><a href="#"><i class="fas fa-file-archive"></i>Documents Archivés</i></a></li>
+        </ul>
+      <!--Navigation bar end-->
+       <!--Copyright-->
+       <div class="copyright">
+        Copyright 2021&copy;Developed By SMACeDOC Team
+      </div>
+      <!--Copyright end-->
+<!-- End SideBar -->
+</div>
