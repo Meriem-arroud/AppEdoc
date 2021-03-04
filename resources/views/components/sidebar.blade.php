2ef48f80f96198729cfd68d811f781a19e0d1128
Chaymae Benayad <chaymae.benayad@etu.uae.ac.ma>
2021-03-04 01:54:58 +0100

    update sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,8 +7,9 @@
             <li><a href="ProfilAdmin"><i class="fas fa-home"></i>Accueil</i></a></li>
             <li><a href="addfileAdmin"><i class="fas fa-plus-circle"></i>Ajouter Document</i></a></li>
             <li><a href="getfileAdmin"><i class="fas fa-file-alt"></i>Documents</i></a></li>
-            <li><a href="getUsers"><i class="fas fa-users"></i>Employés</i></a></li>
             <li><a href="getArchivedDocs"><i class="fas fa-file-archive"></i>Documents Archivés</i></a></li>
+            <li><a href="getUsers"><i class="fas fa-users"></i>Employés</i></a></li>
+           
         </ul>
       <!--Navigation bar end-->
        <!--Copyright-->
