122a3298f933bcf241ed33abf2f167f1ab1bbe3f
Meriem-arroud <arroudmeriem@gmail.com>
2021-03-06 17:42:35 +0100

    some updates

--- resources/views/profile.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>user page </h1>
-<h3>  Hi {{ session('user')->nom}} {{ session('user')->prenom}}</h3>
-
-
-<td><a href="addfile"><button type="button">add file</button></a></td>
-<td><a href="getfile"><button type="button">get file</button></a></td>
-<a href="logout" >déconnection</a>
